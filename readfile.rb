2b3dbb1a758aa25ba7622b4aa47f689ad48f0b14
AndriiKot <a_kocyuba@ukr.net>
2023-08-16 10:10:35 +0100

    create new file 'NewSentencesDEMO.txt'

--- readfile.rb
+++ readfile.rb
@@ -1,9 +1,16 @@
 ﻿array = File.readlines('sentencesDEMO.txt',chomp: true)
 # p array
-p array.size
+# p array.size
 array.delete_if { |el| el =~ /\d+/}
-p array.size
-p array
+# p array.size
+File.open('NewSentencesDEMO.txt','w+') do |file|
+
+end
 
 stringFile = array.join
+p stringFile.size
+stringFile.gsub!(/[:,!.\\=?"()]/,' ')
+p stringFile.size
 p stringFile
+
+
