aa74f492b223ebd39bf9c9a83da0af781a41f87a
AndriiKot <a_kocyuba@ukr.net>
2023-08-16 10:39:57 +0100

    testing newArray

--- readfile.rb
+++ readfile.rb
@@ -4,8 +4,16 @@
 array.delete_if { |el| el =~ /\d+/}
 # p array.size
 newArray  = array.dup
+
 p newArray.object_id
 p array.object_id
+p array.size,newArray.size
+p "-" * 33
+
+p newArray.size
+newArray.delete('')
+p newArray.size
+
 File.open('NewSentencesDEMO.txt','w+') do |file|
     file.puts *array.join.split('.').map!{|el| el + '.' if not el.empty?}
 end
