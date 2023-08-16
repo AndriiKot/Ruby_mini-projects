a06890e1c9df0427d46c8f48e21d6e81e404917d
AndriiKot <a_kocyuba@ukr.net>
2023-08-16 10:51:20 +0100

    testing newArray

--- readfile.rb
+++ readfile.rb
@@ -3,7 +3,7 @@
 # p array.size
 array.delete_if { |el| el =~ /\d+/}
 # p array.size
-newArray  = array.dup
+newArray  = array.dup.join.split('.')
 
 p newArray.object_id
 p array.object_id
@@ -11,11 +11,16 @@ p array.size,newArray.size
 p "-" * 33
 
 p newArray.size
-newArray.delete('')
 p newArray.size
+p newArray
+puts newArray
+
+newArray.map {|el| el.split("!")}
+
+puts newArray
 
 File.open('NewSentencesDEMO.txt','w+') do |file|
-    file.puts *array.join.split('.').map!{|el| el + '.' if not el.empty?}
+    file.puts *newArray.map!{|el| el + '.' if not el.empty?}
 end
 
 stringFile = array.join
@@ -24,6 +29,6 @@ stringFile.gsub!(/[:,!.\\=?"()]/,' ')
 # p stringFile.size
 # p stringFile
 
-#gets
+gets
 
 
