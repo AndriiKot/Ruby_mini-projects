39fdffa4c70370abc7cb77e6e033e366a2e57c34
AndriiKot <a_kocyuba@ukr.net>
2023-08-16 10:37:24 +0100

    create clone newArray

--- readfile.rb
+++ readfile.rb
@@ -3,15 +3,18 @@
 # p array.size
 array.delete_if { |el| el =~ /\d+/}
 # p array.size
+newArray  = array.dup
+p newArray.object_id
+p array.object_id
 File.open('NewSentencesDEMO.txt','w+') do |file|
-    file.puts *array.join.split('.').map!{|el| el + '.' not !el.empty?}
+    file.puts *array.join.split('.').map!{|el| el + '.' if not el.empty?}
 end
 
 stringFile = array.join
-p stringFile.size
+# p stringFile.size
 stringFile.gsub!(/[:,!.\\=?"()]/,' ')
-p stringFile.size
-p stringFile
+# p stringFile.size
+# p stringFile
 
 #gets
 
