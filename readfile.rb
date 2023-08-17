410fbd86afe4f4e6a4b3f03a6b30fce96ca475fa
AndriiKot <a_kocyuba@ukr.net>
2023-08-17 10:05:29 +0100

    create NewUniqWords.txt

--- readfile.rb
+++ readfile.rb
@@ -1,21 +1,25 @@
-﻿array = File.readlines('sentencesDEMO.txt',chomp: true)
+﻿array = File.readlines('SentencesORIGINAL.txt',chomp: true)
 array.delete_if { |el| el =~ /\d+/}
 newArray  = array.dup.join.split('.')
 
 
-File.open('NewSentencesDEMO.txt','w+') do |file|
+File.open('NewSentences.txt','w+') do |file|
     file.puts *newArray.map!{|el| el + '.' if not el.empty?}
 end
 
 stringFile = array.join
 stringFile.gsub!(/[:,!.\\=?"()]/,' ')
 
-p stringFile
 arrWords = stringFile.split(' ');
-p arrWords
-p arrWords.size
 arrWords.uniq!
-p arrWords.size
+
+File.open('NewUniqWords.txt','w+') do |file|
+    file.puts *arrWords
+    file.puts "-" * 145
+    file.puts "words: #{arrWords.size}"
+end
+
+
 
 
 
