d54ca2f1a8acbc9a1e633b21756601bd640fe915
AndriiKot <a_kocyuba@ukr.net>
2023-08-18 10:26:25 +0100

    testing original file

---
+++ Ruby/readFiles/readfile.rb
@@ -0,0 +1,52 @@
+﻿arraySentencesOriginal = File.readlines('SentencesORIGINAL.txt',chomp: true)
+arraySentencesOriginal.delete_if { |el| el =~ /\d+/ }
+newArraySentences = arraySentencesOriginal.dup.join.split('.')
+p newArraySentences
+
+
+File.open('NewSentences.txt','w+') do |file|
+    file.puts *newArraySentences.map!{|el| el + '.' if not el.empty?}
+end
+
+# stringFile = array.join
+# stringFile.gsub!(/[:,!.\\=?"()]/,' ')
+
+# arrWords = stringFile.split(' ');
+# arrWords.uniq!
+
+# File.open('NewUniqWords.txt','w+') do |file|
+#     file.puts *arrWords
+#     file.puts "-" * 145
+#     file.puts "words: #{arrWords.size}"
+# end
+
+# arrayTransletion = File.readlines('TransletionG.txt', chomp: true)
+
+# arrayWordsPlusTaransletion = arrWords.zip(arrayTransletion)
+# hasharrayWordsPlusTaransletion = arrayWordsPlusTaransletion.to_h
+
+# arraySentences = File.readlines('NewSentences.txt',chomp: true)
+# arraySentences.compact!
+# arraySentences.delete_if {|el| el == ''}
+
+# File.open('DEMO.txt',"w+") do |file|
+#     file.puts *arraySentences
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
