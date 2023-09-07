914a3611fd1e5980990ccd80058788b73ada60a0
AndriiKot <a_kocyuba@ukr.net>
2023-09-07 23:05:33 +0100

    optimization code break

--- Ruby/Project/readFiles/DEMO/constructorJSON.rb
+++ Ruby/Project/readFiles/DEMO/constructorJSON.rb
@@ -5,10 +5,9 @@ def create_element_name() = my_break(gets.strip)
 def my_break(str) = str == '' ? false : str 
 
 loop do
-    element_key = create_element_name || break
-    element_value = create_element_name || break
+    element_key = create_element_name 
+    element_value = create_element_name 
+    element_key && element_value || break
     hash_Folders[element_key] = element_value
 end
 
-hash_Folders.delete_if {|key, value| key == '' or value == ''}
-p hash_Folders
