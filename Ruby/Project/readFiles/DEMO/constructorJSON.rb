72bbc4697c8d99e297a0391dc845a809dcf7f1aa
AndriiKot <a_kocyuba@ukr.net>
2023-09-07 13:53:00 +0100

    testing new failes

---
+++ Ruby/Project/readFiles/DEMO/constructorJSON.rb
@@ -0,0 +1,16 @@
+﻿hash_Folders = {}
+
+hash_Folders.default = 'test'
+p hash_Folders
+p hash_Folders['bar']
+counts = {foo: 42}
+p counts.default = '' # => nil (default)
+counts[:foo] = 42
+
+p counts[:bar]
+
+
+
+
+
+
