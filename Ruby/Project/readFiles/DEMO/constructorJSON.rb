972043c0cbca8e15c870dc58845d83e6bd3dd441
AndriiKot <a_kocyuba@ukr.net>
2023-09-08 08:30:13 +0100

    create new methods

--- Ruby/Project/readFiles/DEMO/constructorJSON.rb
+++ Ruby/Project/readFiles/DEMO/constructorJSON.rb
@@ -1,13 +1,22 @@
 ﻿hash_Folders = {}
 hash_Folders.default = ''
 
-def create_element_name() = my_break(gets.strip)
-def my_break(str) = str == '' ? false : str 
+def create_element = print "Create Folder or File?('d'/'f'): "
+def folder_or_file(str) = is_fodler?(str) || is_file?(str)
+def is_fodler(str) = print "is folder"
+def is_file(str) =  print "is file"
+def enter_name(str) = print "Enter name #{str}: "
 
-loop do
-    element_key = create_element_name 
-    element_value = create_element_name 
-    element_key && element_value || break
-    hash_Folders[element_key] = element_value
-end
+
+# def create_element_name() = my_break(gets.strip)
+# def empty_str?(str) = str == '' ? true : fasle 
+
+# def make_Folder_or_File 
+# loop do
+#     element_key = create_element_name 
+#     element_value = create_element_name 
+#     element_key && element_value || break
+#     hash_Folders[element_key] = element_value
+# end
+# end
 
