a07e590982a242d84d14d6766c2318d4c7cc2f96
AndriiKot <a_kocyuba@ukr.net>
2023-09-09 21:40:21 +0100

    add new methods

--- Ruby/Project/readFiles/DEMO/constructorJSON.rb
+++ Ruby/Project/readFiles/DEMO/constructorJSON.rb
@@ -3,38 +3,26 @@ def print_ques_file = print "Create File?('Y/N'): "
 def print_ques_folder = print "Create Folder?('Y/N'): "
 def print_ques_name(str) = print "Enter name #{str}: "
 def yes_?(str) = str.casecmp? 'y' || str.casecmp? 'yes'
+def is_empty?(str) = '' if str.empty?
+
+
 
 def print_next_step = print "Push Enter to next step."
-def print_wrong_data_folder_or_file 
-    puts "Wrong data"
-    print_question
-    print "\n or \n"
-    print_next_step
-    print "\n"
-end
-
-def is_file?(str) 
-    'file' if str.casecmp?('file') || str.casecmp?('f')
-end
-
-def is_empty?(str)
-    '' if str.empty?
-end
-
-def is_folder?(str)
-    'folder' if str.casecmp?('folder') || str.casecmp?('dir') || str.casecmp?('d')
-end
 
 
-print_wrong_data_folder_or_file 
-loop do
-    print_question
-    get_answer = gets.strip
-    answeer_folder_or_file = is_file?(get_answer) || is_folder?(get_answer) || is_empty?(get_answer)
-    p answeer_folder_or_file
-    next unless answeer_folder_or_file
-    break if answeer_folder_or_file == ''
-end
+
+
+
+
+
+# print_wrong_data_folder_or_file 
+# loop do
+#     get_answer = gets.strip
+#     answeer_folder_or_file = is_file?(get_answer) || is_folder?(get_answer) || is_empty?(get_answer)
+#     p answeer_folder_or_file
+#     next unless answeer_folder_or_file
+#     break if answeer_folder_or_file == ''
+# end
 
 
 # def create_element_name() = my_break(gets.strip)
