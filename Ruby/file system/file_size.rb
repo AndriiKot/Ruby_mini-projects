3cef5a0d333d290c63c83673c8e10ec2c1379352
AndriiKot <a_kocyuba@ukr.net>
2024-02-03 20:57:45 +0100

    create project 'folder size'

---
+++ Ruby/file system/file_size.rb
@@ -0,0 +1,9 @@
+﻿
+def folder_size_bytes(folder_path)
+    total_size = 0
+    Dir.glob(File.join(folder_path, '**', '*')).each do |file|
+      next if File.directory?(file) 
+      total_size += File.size(file)
+    end
+    total_size
+end
