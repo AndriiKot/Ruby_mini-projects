4b8dc146885e3ed0e1aa05736ab7c92209999488
AndriiKot <a_kocyuba@ukr.net>
2024-02-10 20:58:06 +0100

    create rubyScript 'tree_Codewars'

---
+++ Ruby/tree_Codewars/createCodewarsTree.rb
@@ -0,0 +1,22 @@
+﻿main_folder = ARGV[0]
+
+p main_folder
+def createTreeCodewars(main_folder = 'main_folder')
+    if Dir.exist?("./")
+        p "#{main_folder}: exist? Yes"
+    else
+        p "#{main_folder}: exist? No"
+    end
+# Dir.mkdir(main_folder)
+
+# (1..8).each do |i|
+#     folder_name = "#{i}"
+#     Dir.mkdir(folder_name)
+  
+#     # Создание файла .gitkeep внутри каждой папки
+#     File.open("#{folder_name}/.gitkeep", 'w') {}
+#   end
+end
+
+createTreeCodewars(main_folder)
+  
