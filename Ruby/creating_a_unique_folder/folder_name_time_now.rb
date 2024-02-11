e23e8c5dce4f3fa0961651d73e1be5c6c660e6e1
AndriiKot <a_kocyuba@ukr.net>
2024-02-11 02:13:08 +0100

    create script unigque folder name 'time now'

---
+++ Ruby/creating_a_unique_folder/folder_name_time_now.rb
@@ -0,0 +1,19 @@
+﻿
+folder_name = ARGV[0]   
+   
+def create_uniq_folder(folder_name = nil)
+    folder_name  ||= 'folder_name'
+   if Dir.exist?("./#{folder_name}")
+        suffix = Time.now.to_f.to_s.gsub('.','')
+        loop do
+           folder_path = "#{folder_name}#{suffix}"
+            unless Dir.exist? folder_path
+                return Dir.mkdir(folder_path)
+            end
+            suffix = (suffix.to_i + 1).to_s
+        end
+    end
+   Dir.mkdir(folder_name)
+end
+
+create_uniq_folder(folder_name)
