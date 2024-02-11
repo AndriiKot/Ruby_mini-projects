b7eebd9436c4bc3416d929a9d85193e50d346287
AndriiKot <a_kocyuba@ukr.net>
2024-02-11 23:54:22 +0100

    create rubyScript 'tree_Codewars_v_1_0_1'

--- Ruby/creating_a_unique_folder/folder_name_time_now.rb
+++ Ruby/creating_a_unique_folder/folder_name_time_now.rb
@@ -4,7 +4,7 @@ folder_name = ARGV[0]
 def create_uniq_folder(folder_name = nil)
     folder_name  ||= 'folder_name'
    if Dir.exist?("./#{folder_name}")
-        suffix = Time.now.to_f.to_s.gsub('.','')
+        suffix = Time.now.to_i.to_s.gsub('.','')
         loop do
            folder_path = "#{folder_name}#{suffix}"
             unless Dir.exist? folder_path
---
+++ Ruby/tree_Codewars/createCodewarsTree_version_1_0_0.rb
@@ -0,0 +1,24 @@
+﻿
+def create_tree_folders_Codewars
+    path = "./Codewars"
+    Dir.mkdir(path)
+    Dir.chdir(path)
+
+    ARGV.each do |main_folder|
+        Dir.mkdir(main_folder)
+        (1..8).each do |i|
+             path = "./#{main_folder}/kyu_#{i}"
+             Dir.mkdir(path)
+             File.open("#{path}/.gitkeep",'w'){}
+        end
+    end
+end
+
+create_tree_folders_Codewars()
+
+# TEST:
+# Create New Project "Codewars"
+# $ ruby createCodewarsTree_version_1_0_0.rb ruby javascript go scala java c c++ rust 
+#
+
+
