b31f056fa816eee4bc71613d839495e5ec1d8cc6
AndriiKot <a_kocyuba@ukr.net>
2024-02-14 14:02:56 +0100

    create version 1.0.0 createBranches and deleteBranches

---
+++ Ruby/tree_Codewars/createCodewarsTree_version_1_0_2.rb
@@ -0,0 +1,19 @@
+﻿
+def create_tree_folders_Codewars
+    ARGV.each do |main_folder|
+        Dir.mkdir(main_folder) unless Dir.exist?(main_folder)
+        (1..8).each do |i|
+             path = "./#{main_folder}/kyu-#{i}"
+             Dir.mkdir(path) unless Dir.exist?(path)
+             File.open("#{path}/.gitkeep",'w'){} if Dir.empty?(path)
+        end
+    end
+end
+
+create_tree_folders_Codewars()
+
+# TEST:
+# Adding to an existing project 'Codewars'
+# $ ruby createCodewarsTree_version_1_0_1.rb ruby javascript go scala java c c++ rust 
+#
+
