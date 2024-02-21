c5e849bb0a027d7c2c32aaf1f85a05be4078857f
AndriiKot <a_kocyuba@ukr.net>
2024-02-21 12:35:57 +0100

    test rational numbers

---
+++ Ruby/_ProjectsInDeveloping/number/is_number_v2.rb
@@ -0,0 +1,20 @@
+﻿class String
+  def to_integer
+    Integer(self) rescue nil
+  end
+ 
+  def to_float
+    Float(self) rescue nil
+  end
+
+  def to_rational
+    Rational(self) rescue nil
+  end
+  
+  def to_number
+    to_integer || to_float|| to_rational
+  end
+end
+
+#TEST:
+puts "string 5/4 containing an 2/3 integer 13 and a float 24.0".split(' ').filter(&:to_number).map!(&:to_i).sum
