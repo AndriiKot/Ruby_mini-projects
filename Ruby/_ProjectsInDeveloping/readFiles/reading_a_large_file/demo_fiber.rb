c01945bbe054ec16f61baf09e8aab9bed59f7c5b
AndriiKot <a_kocyuba@ukr.net>
2024-02-21 23:22:18 +0100

    fiber fixing

--- Ruby/_ProjectsInDeveloping/readFiles/reading_a_large_file/demo_fiber.rb
+++ Ruby/_ProjectsInDeveloping/readFiles/reading_a_large_file/demo_fiber.rb
@@ -2,24 +2,19 @@
 
 file = File.open("input.txt", "r")
 sum = 0
-fiber = Fiber.new do
+fiber = Fiber.new do 
   loop do
     data = file.read(1000) # Читаем файл по частям
-    p data
-    p data.end_with?("\n")
-    n = data.end_with?("\n") ? 0 : data.match(/\d+$/)[0]
-    p n
-    gets
+    line = data.encode('UTF-8', 'UTF-8') if data
+    numbers = line.scan(/\d+/).map!(&:to_i) if data 
+    sum += numbers&.sum  if numbers
     Fiber.yield(data) # Передаем часть данных обратно
   end
 end
 
 loop do
-  data = fiber.resume n
+  data = fiber.resume 
   break if data.nil?
-  
-  # Обрабатываем данные
-  # puts data
 end
 
 file.close
