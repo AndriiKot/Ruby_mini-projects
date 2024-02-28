7e8c02cc9c16a386cd4d8a62a1696aaa67a0b85d
AndriiKot <a_kocyuba@ukr.net>
2024-02-28 09:05:24 +0100

    create new project 'Removing spaces and illegal                     characters'

---
+++ Folders_Operations/removing_spaces_and_illegal_characters/removing_space_0_0_1.rb
@@ -0,0 +1,22 @@
+﻿
+dir = Dir.new('./')
+
+dir.each_child  do |folder| 
+    new_folder_name = ''
+    regex = /[^\w\.]/
+    p folder
+    if folder.match?(regex)
+        folder.strip.each_char do |chr|
+            if chr.match?(/-/) || chr.match?('\s')
+                chr.replace('_')
+                new_folder_name += chr
+                next
+            end
+              next if chr.match?(regex) 
+              new_folder_name += chr
+        end
+        File.rename(folder, new_folder_name)
+    else
+    end
+end
+
