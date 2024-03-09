437a59fad1bd7d11c2bd730ed735a162273c51fb
AndriiKot <a_kocyuba@ukr.net>
2024-03-09 22:40:23 +0100

    version 2.0.0 finished removing spaces and illegal     characters

--- Folders_Operations/removing_spaces_and_illegal_characters/removing_space_1_0_0.rb
+++ Folders_Operations/removing_spaces_and_illegal_characters/removing_space_1_0_0.rb
@@ -4,4 +4,3 @@ Dir.new('./').each_child do |folder_or_file|
   new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w\.]/,'_')  
   File.rename(folder_or_file, new_folder_or_file_name)
 end
-  
---
+++ Folders_Operations/removing_spaces_and_illegal_characters/removing_space_2_0_0.rb
@@ -0,0 +1,13 @@
+﻿
+def removing_space_all_depth(folder_path = './')
+  Dir.glob(File.join(folder_path,"**","*")) do |path|
+    if File.directory?(path)
+      Dir.new(path).each_child do |folder_or_file|
+        new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w[а-яА-Я]\.]/,'_')  
+        File.rename(File.join(path,folder_or_file), File.join(path,new_folder_or_file_name))
+      end
+    end
+  end
+end
+
+removing_space_all_depth()
