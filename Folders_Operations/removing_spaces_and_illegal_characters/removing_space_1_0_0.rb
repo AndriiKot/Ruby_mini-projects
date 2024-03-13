229fdddf10f06fa5b23450873e986d8c359ee5a7
AndriiKot <a_kocyuba@ukr.net>
2024-03-13 14:40:30 +0100

    print result to cmd

--- Folders_Operations/removing_spaces_and_illegal_characters/removing_space_1_0_0.rb
+++ Folders_Operations/removing_spaces_and_illegal_characters/removing_space_1_0_0.rb
@@ -1,6 +1,9 @@
 ﻿
 
 Dir.new('./').each_child do |folder_or_file|
-  new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w\.]/,'_')  
+  new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w\.]/,'_') 
+  p folder_or_file
+  p new_folder_or_file_name
+  p "*" * 23
   File.rename(folder_or_file, new_folder_or_file_name)
 end
--- Folders_Operations/removing_spaces_and_illegal_characters/removing_space_2_0_0.rb
+++ Folders_Operations/removing_spaces_and_illegal_characters/removing_space_2_0_0.rb
@@ -4,6 +4,9 @@ def removing_space_all_depth(folder_path = './')
     if File.directory?(path)
       Dir.new(path).each_child do |folder_or_file|
         new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w[а-яА-Я]\.]/,'_')  
+        p folder_or_file
+        p new_folder_or_file_name
+        p "*" * 23
         File.rename(File.join(path,folder_or_file), File.join(path,new_folder_or_file_name))
       end
     end
