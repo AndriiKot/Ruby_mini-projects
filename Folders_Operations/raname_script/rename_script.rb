cfd5552e03088156ddfce1f1302e17c192ce1963
AndriiKot <a_kocyuba@ukr.net>
2024-04-08 15:56:25 +0200

    create new ruby script 'rename and copy to files'

---
+++ Folders_Operations/raname_script/rename_script.rb
@@ -0,0 +1,19 @@
+﻿require 'fileutils'
+
+target_dir = './'
+destination_dir = 'test'
+
+if !Dir.exist?(destination_dir)
+  FileUtils.mkdir(destination_dir)
+end
+
+Dir.glob("#{target_dir}/**/*").each do |file|
+  next if File.directory?(file)
+
+  file_name = File.basename(file)
+  folder_name = File.basename(File.dirname(file))
+
+  new_file_name = "#{destination_dir}/#{folder_name}_#{file_name}"
+
+  FileUtils.cp(file, new_file_name)
+end
--- Folders_Operations/removing_spaces_and_illegal_characters/Removing_Space_1_0_1.rb
+++ Folders_Operations/removing_spaces_and_illegal_characters/Removing_Space_1_0_1.rb
@@ -3,7 +3,7 @@
 Dir.new('./').each_child do |folder_or_file|
   new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w\.]/,'_') 
   capitalize = new_folder_or_file_name.split('_').map do |word| 
-     word[0] = word[0].upcase; word
+     word[0] = word[0].upcase if word[0]; word
   end.join('_')
   p new_folder_or_file_name,capitalize, '*' * 33
   File.rename(folder_or_file, capitalize)
