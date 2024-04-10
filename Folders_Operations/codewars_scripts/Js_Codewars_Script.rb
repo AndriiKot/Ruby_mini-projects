047bd12f566bcc3a5715ae4ec14c0173afb03dd3
AndriiKot <a_kocyuba@ukr.net>
2024-04-10 23:26:23 +0200

    scripts sql and js

---
+++ Folders_Operations/codewars_scripts/Js_Codewars_Script.rb
@@ -0,0 +1,10 @@
+﻿
+path = ARGV[0]
+path = path ? File.join(Dir.pwd, path) : Dir.pwd
+
+File.open(File.join(path, 'DESCRIPTION.txt'), 'w') do |f|
+end
+File.open(File.join(path, 'solution.js'), 'w') do |f|
+end
+File.open(File.join(path, 'solution_best_pr.js'), 'w') do |f|
+end
