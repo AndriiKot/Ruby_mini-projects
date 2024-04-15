de48343582edae8665edebc14dbf73b2aa085410
AndriiKot <a_kocyuba@ukr.net>
2024-04-15 17:42:04 +0200

    create new ruby script

---
+++ Folders_Operations/codewars_scripts/Removing_Space_1_0_1.rb
@@ -0,0 +1,10 @@
+﻿
+
+Dir.new('./').each_child do |folder_or_file|
+  new_folder_or_file_name = folder_or_file.strip.gsub(/[^\w\.]/,'_') 
+  capitalize = new_folder_or_file_name.split('_').map do |word| 
+     word[0] = word[0].upcase if word[0]; word
+  end.join('_')
+  p new_folder_or_file_name,capitalize, '*' * 33
+  File.rename(folder_or_file, capitalize)
+end
