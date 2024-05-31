c6442b407da62ad06538e2437e2eda773f854875
AndriiKot <a_kocyuba@ukr.net>
2024-05-31 13:57:20 +0200

    RUBY 'Scripts on Ruby' CRAET 'Git subtree English Lesson' ! Testing script number 2 !

---
+++ Git_scripts/subreeEngilshLesson/_02_update_all_projects.rb
@@ -0,0 +1,13 @@
+﻿lessons = ARGV[0].to_i
+repeats = lessons == 0  ? 2 : lessons
+max_value_size = 25.to_s.size
+
+
+repeats.times do |i|
+    lesson_start = i + 1
+    folder = "_#{'0' * (max_value_size - (lesson_start).to_s.size)}#{lesson_start}__Lesson__"
+    git_repo =  "https://github.com/AndriiKot/Lesson_#{lesson_start}__Course_25__A0-A1__.git"
+    system(`git subtree pull --prefix #{folder} #{git_repo} main`)
+end
+
+system(`git push`)
