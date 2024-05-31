e56d7632ec743e5d73b63f9bf2e74c4ca505203a
AndriiKot <a_kocyuba@ukr.net>
2024-05-31 14:06:50 +0200

    RUBY 'Scripts on Ruby' replace_integer.rb update

---
+++ Folders_Operations/raname_files/replace_integer.rb
@@ -0,0 +1,10 @@
+﻿
+
+Dir.new('./').each_child do |folder|
+    replace = '8-kyu'
+    unless folder.include?(replace) && !File.file?(folder)
+        new_folder_name = folder.gsub(/\d+/,replace)
+        File.rename(folder, new_folder_name)
+    end
+end
+
