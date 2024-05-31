5233917b444c8e0c617d5f29344bbbbda1e521ca
AndriiKot <a_kocyuba@ukr.net>
2024-05-31 13:39:46 +0200

    RUBY 'Scripts on Ruby' CRAET 'Git subtree English Lesson'

---
+++ Git_scripts/subreeEngilshLesson/_01_update_all_projects.rb
@@ -0,0 +1,17 @@
+﻿lessons = 25
+max_value_size = lessons.to_s.size
+
+repositories = [
+    'https://github.com/AndriiKot/Lesson_1__Course_25__A0-A1__.git',
+]
+
+update = "git subtree pull --prefix "
+
+
+repositories.each_with_index do |element, index|
+    folder = "_#{'0' * (max_value_size - (index + 1).to_s.size)}#{index + 1}__Lesson__"
+    git_repo = element;
+        system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
