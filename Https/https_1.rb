246f8353d50039eb1cd360d2ca72af0a7a13847e
AndriiKot <a_kocyuba@ukr.net>
2024-06-05 10:31:07 +0200

    Ruby !! Create New Script !!

---
+++ Https/https_1.rb
@@ -0,0 +1,17 @@
+﻿require 'net/http'
+require 'json'
+
+url = URI.parse('https://www.codewars.com/api/v1/users/AndriiKot')
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = (url.scheme == 'https')
+
+request = Net::HTTP::Get.new(url.request_uri)
+
+response = http.request(request)
+
+if response.code == '200'
+  data = JSON.parse(response.body)
+  puts data
+else
+  puts "HTTP request failed: #{response.code} #{response.message}"
+end
